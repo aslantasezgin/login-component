19ee8ccf997391272da6d6093516e6c20498d23d
aslantasezgin <aslantasezgin@gmail.com>
2022-10-24 16:26:11 +0300

    Added form and made style adjustments

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import Input from './components/Input/Input';
 import { useState } from 'react';
 import Button from './components/Button/Button';
-
+import Form from './components/Form/Form';
 function App() {
 
   const[text,setText] = useState("Sezgin")
 
   return (
+
+    
     <div className="App">
+      <Form>
+        <h2>Login Page</h2>
       <Input value={text} placeholder={"Metin Giriniz"} onChange={(e) => {
           setText(e.target.value)
       }} />
+
+<Input value={text} placeholder={"Metin Giriniz"} onChange={(e) => {
+          setText(e.target.value)
+      }} />
+
       <Button text={"Giriş Yap"} onClick={console.log('Tıklandı')}></Button>
+      </Form>
     </div>
   );
 }
